2e00060ef8f4f74ffd2cb649c8f917e651fb5f9a
mutecypher <mutecypher@icloud.com>
2022-12-22 15:47:59 -0800

    second

--- Gen_openaii.py
+++ Gen_openaii.py
@@ -1,9 +1,10 @@
 import openai
 import os
+import sys
+import api
 
-api_secret_key = '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'
-
-openai.api_key = api_secret_key
+OPENAI_API_KEY = api.open_key()
+openai.api_key = OPENAI_API_KEY
 
 lynxie = openai.Image.create(
     prompt="Ogre giving flower to small girl", n=1, size="1024x1024")
