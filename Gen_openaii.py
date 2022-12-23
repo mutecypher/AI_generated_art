9bb3293d2c1c67e917caee950cf460de290363f4
mutecypher <mutecypher@icloud.com>
2022-12-22 16:14:01 -0800

    Update Gen_openaii.py

--- Gen_openaii.py
+++ Gen_openaii.py
@@ -1,14 +1,34 @@
 import openai
-import os
-import sys
+import webbrowser
 import api
 
 OPENAI_API_KEY = api.open_key()
 openai.api_key = OPENAI_API_KEY
 
-lynxie = openai.Image.create(
-    prompt="Ogre giving flower to small girl", n=1, size="1024x1024")
+print()
+print()
+print("Welcome to the OpenAI Image Generator!")
+print()
+print()
 
-image_url = lynxie['data'][0]['url']
+keep_on = 'y'
+while keep_on == 'y':
+    print()
+    print()
+    prompty = input("Enter your prompt for an image to create: ")
 
-print(image_url)
+    lynxie = openai.Image.create(
+        prompt=prompty, n=1, size="1024x1024")
+
+    image_url = lynxie['data'][0]['url']
+
+# print(image_url)
+
+    webby = webbrowser.open_new(image_url)
+
+    print()
+    keep_on = input("Would you like to continue? (y/n): ")
+
+print()
+print()
+print("Thank you for using the OpenAI Image Generator!")
