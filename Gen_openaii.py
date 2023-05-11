6a322380af3c3d44931cceb92a69cb789a8a5c58
mutecypher <mutecypher@icloud.com>
2023-05-11 12:23:03 -0700

    mo stuff

--- Gen_openaii.py
+++ Gen_openaii.py
@@ -1,9 +1,14 @@
 import openai
 import webbrowser
 import api
+import urllib.request as ulr
+import os
+import ssl
 
 OPENAI_API_KEY = api.open_key()
 openai.api_key = OPENAI_API_KEY
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 print()
 print()
@@ -15,19 +20,27 @@ while keep_on == 'y':
     print()
     print()
     prompty = input("Enter your prompt for an image to create: ")
-
+    n = int(input('and how many?   '))
+    folder_path = '/Volumes/Elements/GitHub/cats_with_birds/For_Training/gen_ai/'
+    image_prefix = str(prompty.replace(" ", "_"))
+    ##image_count = 0
     lynxie = openai.Image.create(
-        prompt=prompty, n=1, size="1024x1024")
+        prompt=prompty, n=n, size="1024x1024")
+    for i in range(n):
+        image_url = lynxie['data'][i]['url']
+        ##webby = webbrowser.open_new(image_url)
+        ulr.urlretrieve(lynxie['data'][i]['url'], folder_path + image_prefix + str(i) + '.jpg')
 
-    image_url = lynxie['data'][0]['url']
-
-# print(image_url)
-
-    webby = webbrowser.open_new(image_url)
+    
 
     print()
-    keep_on = input("Would you like to continue? (y/n): ")
+    print("the number saved is ", n)
+    keep_on = input("Do you want to continue? y/n  ")
 
 print()
 print()
 print("Thank you for using the OpenAI Image Generator!")
+
+
+
+
