8fe217abfc3613f4509400d1a71d065b8ac74842
mutecypher <mutecypher@icloud.com>
2023-09-28 19:50:09 -0700

    Update Gen_openaii.py

--- Gen_openaii.py
+++ Gen_openaii.py
@@ -16,31 +16,54 @@ print("Welcome to the OpenAI Image Generator!")
 
 
 colors = [
-    ##"brown", 
-    ##"white", 
+
+    ##, 
+    "white",
+        "black",
     "grey",
-    "brown", 
-    ##"striped", 
-    ##"orange", 
-    "black"
+        "brown",
+    ##, "spotted"
     ]
 animals = [ 
-           "raccoon",
-           "squirrel",
-            "chipmunk",
-            "bear",
-            "linx",
-            "skunk",
-            "feral pig"
+           
+
+
+            "yorkshire terrier",
+            "pomeranian",
+            ##"corgie",
+            "pooch",            
+             "great dane",
+            "boxer",
+            "poodle",
+            "shih tzu",
+            "pit bull",
+            "maltese",
+            "poodle",
+            "bulldog",
+            "labrador",
+            "golden retriever",
+            "german shepherd",
+            "husky",
+            "rottweiler",
+            "beagle",
+            "dachshund",
+            "chihuahua"
+            
            ]
-poses = ["facing camera", "walking to camera",
-          "looking left", "looking right"
+poses = [ "facing forward on deck",
+          "facing forward on lawn", "facing forward in garage"
          ]
 mouths =[
-    "mouth closed",  
-   ## "lizard in mouth", ##"rat in mouth",
-    ## "mouse in mouth", "bird in mouth"
-    "mouth open"
+    ##"mouth closed",  
+    ##"lizard in mouth",
+    "with a rat in mouth",
+    ##"mouse in mouth",
+    "with bird in mouth",
+    
+    ##"closed mouth"
+    ##,
+    "with a stick in mouth"
+    ##"panting"
     ]
 
 for color in colors:
@@ -48,8 +71,8 @@ for color in colors:
         for pose in poses:
             for mouth in mouths:
                 n = 10
-                prompt = f"{color} {animal} {pose} {mouth}"
-                folder_path = '/Volumes/Elements/GitHub/cats_with_birds/For_Training/non_cat/'
+                prompt = f"{color} {animal}  {pose} {mouth}"
+                folder_path = '/Volumes/Elements/GitHub/cats_with_birds/For_Training/dog_open/'
                 image_prefix = str(prompt.replace(" ", "_"))
     ##image_count = 0
                 lynxie = openai.Image.create(prompt= image_prefix, n=n, size="256x256")
@@ -60,32 +83,11 @@ for color in colors:
                     ulr.urlretrieve(lynxie['data'][i]['url'], folder_path + image_prefix + str(i) + '.jpg')
 
 
-print("competed")
+print("completed")
 
 
 
 
-keep_on = 'y'
-while keep_on == 'y':
-    print()
-    print()
-    prompty = input("Enter your prompt for an image to create: ")
-    n = int(input('and how many?   '))
-    folder_path = '/Volumes/Elements/GitHub/cats_with_birds/For_Training/gen_ai/'
-    image_prefix = str(prompty.replace(" ", "_"))
-    ##image_count = 0
-    lynxie = openai.Image.create(
-        prompt=prompty, n=n, size="256x256")
-    for i in range(n):
-        image_url = lynxie['data'][i]['url']
-        ##webby = webbrowser.open_new(image_url)
-        ulr.urlretrieve(lynxie['data'][i]['url'], folder_path + image_prefix + str(i) + '.jpg')
-
-    
-
-    print()
-    print("the number saved is ", n)
-    keep_on = input("Do you want to continue? y/n  ")
 
 print()
 print()
